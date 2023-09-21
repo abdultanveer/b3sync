3235b7797ce052bfe0ac17dd02d979f94fdb45a5
Abdul <abdul.tanveer@gmail.com>
2023-09-21 14:36:07 +0530

    logging-logcat

--- app/src/main/java/com/example/b3sync/MainActivity.kt
+++ app/src/main/java/com/example/b3sync/MainActivity.kt
@@ -2,34 +2,42 @@ package com.example.b3sync
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.b3sync.data.Dice
 import com.google.android.material.snackbar.Snackbar
+import java.lang.NullPointerException
 
 //class MainActivity extends AppCompatActivity
 class MainActivity : AppCompatActivity() {
-
+private  val TAG = "MainActivity"
     lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  //inflation --layout inflater
-
+        Log.i(TAG,"activity created")
         rollButton = findViewById(R.id.btnRoll) //taking handle of button
         rollButton.setOnClickListener {
+            Log.e(TAG,"button clicked")
+
             rollDice()
         }
     }
 
     fun rollDice() {
+        Log.w(TAG,"dice was rolled")
+
         var tvRoll: TextView = findViewById(R.id.tvRoll) //taking handle
         var ivDice: ImageView = findViewById(R.id.ivDice)
         ivDice.setImageResource(R.drawable.dice_3)
         var dice = Dice(sides = 6)
         val diceRoll = dice.roll()
+        Log.i(TAG,"you got $diceRoll")
+
 
         tvRoll.setText("you got $diceRoll")
         rollButton.setText("dice rolled")
@@ -54,6 +62,6 @@ class MainActivity : AppCompatActivity() {
            else -> { 0}
        }
         ivDice.setImageResource(imagetobeSet)
-
+       // throw NullPointerException("simply crashing the app")
         }
     }
